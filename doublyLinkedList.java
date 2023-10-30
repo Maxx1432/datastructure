cc4c616d8a2339f69526093161a0c96edad45298
Mahendra <55547815+Maxx1432@users.noreply.github.com>
2023-10-30 16:01:14 +0530

    Update doublyLinkedList.java file
    add the defination of DLL and add the method
    * Insert at the Begining 
    * Insert at the End
    * Print in Forward Direction
    * Print in Backward Direction

--- doublyLinkedList.java
+++ doublyLinkedList.java
@@ -1,1 +1,104 @@
+/* 
+    null <-- [previous|data|next] <--> [previous|data|next] <--> [previous|data|next] <--> [previous|data|next] --> null
+                          ^                                                                           ^
+                         Head                                                                        Tail
 
+  In Doubly Linked list The node contain address of next node as well as addess of previous node.
+  We can add element, remove element and traverse the Doubly Linked List in both forward and Backward direction.
+  In Doubly Linked List we can delete the node even if we don't have pointer to its previous node.
+  
+*/
+public class DoublyLinkedList {
+
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+  // For creating a node in DLL
+    private class ListNode {
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+  // to check the DLL conatin data or not
+    public boolean isEmpty() {
+        return length ==0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+  // Insert at the begining of the Doubly Linked List
+    public void insertFirst(int value) {
+      ListNode newNode = new ListNode(value);
+        if(isEmpty()){
+            tail = newNode;
+        } else {
+            head.previous = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+        length++;
+    } 
+// Insert at the end of Doubly Linked List
+    public void insertLast(int value) {
+        ListNode newNode = new ListNode(value);
+        if(isEmpty()){
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+  // Print DLL in Forward direction
+    public void displayForward() {
+        if(head == null)
+            return;
+
+        ListNode temp = head;
+        while(temp != null) {
+            System.out.print(temp.data+" --> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+  // Print DLL in Backward direction
+    public void displayBackward() {
+        if(tail == null)
+            return;
+
+        ListNode temp = tail;
+        while (temp != null) {
+            System.out.print(temp.data+" --> ");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String [] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertLast(1);
+        dll.insertLast(10);
+        dll.insertLast(15);
+        dll.insertLast(25);
+
+        dll.displayForward();
+
+        dll.displayBackward();
+    }
+}
