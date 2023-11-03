5f1ffe972c731b2cec717c1c7c37a7636121103d
Mahendra <55547815+Maxx1432@users.noreply.github.com>
2023-11-03 14:44:46 +0530

    Update CircularLinkedList.java
    add mehtod for
    * Node Creation
    * Circular Linked List Creation 
    * Main mehtod

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -11,4 +11,51 @@ Instead of head, we keep track of last node in Circular Singly Linked List
   
 public class CircularLinkedList {
 
+    private ListNode last;
+    private int length;
+
+    private class ListNode{
+        private ListNode next;
+        private int data;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+    }
+
+    public CircularLinkedList(){
+        last = null;
+        length = 0;
+    }
+
+    public int length(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    // Create node and connect them to make a Circular linked list
+    // assign the last
+    public void createCircularLinkedList(){
+
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(10);
+        ListNode third = new ListNode(100);
+        ListNode fourth = new ListNode(1000);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+
+        last = fourth;
+
+    }
+
+    public static void main(String [] args){
+
+        CircularLinkedList cll = new CircularLinkedList();
+        cll.createCircularLinkedList();
+    }
 }
