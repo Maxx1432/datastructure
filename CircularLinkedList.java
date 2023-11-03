fde89280447417bebb6b3f549a3c7cb613397eab
Mahendra <55547815+Maxx1432@users.noreply.github.com>
2023-11-03 13:13:55 +0530

    Update and rename circularLinkedList.java to CircularLinkedList.java
    Add Discription of Circular Linked List

---
+++ CircularLinkedList.java
@@ -0,0 +1,14 @@
+/*
+In Circular Linked List the last node points to first node and not null.
+Instead of head, we keep track of last node in Circular Singly Linked List
+
+                                                                      Last
+                                                                        V
+  --> [Data|addreess] --> [Data|addreess] --> [Data|addreess] --> [Data|addreess] --
+  | <-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- |
+
+*/
+  
+public class CircularLinkedList {
+
+}
