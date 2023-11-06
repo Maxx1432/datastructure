2125f3fc98094e7fa6e995aeee5cab3f52435432
Mahendra <55547815+Maxx1432@users.noreply.github.com>
2023-11-06 16:43:49 +0530

    Update Stack.java
    Add methods
    Push
    Pop
    Peek

--- Stack.java
+++ Stack.java
@@ -25,7 +25,46 @@ public class Stack {
         return length == 0;
     }
 
+    // To insert a element in the stack
+    public void push(int data){
+        ListNode temp = new ListNode(data);
+        temp.next = top;
+        top = temp;
+        length++;
+    }
+
+    // To remove the element from stack
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        int result = top.data;
+        top = top.next;
+        length--;
+        return result;
+    }
+
+    // To check the top element of stack
+    public int peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        } else {
+            return top.data;
+        }
+    }
+
     public static void main(String [] args){
+
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(10);
+        stack.push(100);
+        stack.push(1000);
+
+        System.out.println(stack.peek());
+
+        stack.pop();
+        System.out.println(stack.peek());
 
     }
 }
