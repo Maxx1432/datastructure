7d076137326ee66f22094270deac23843c475265
Mahendra <55547815+Maxx1432@users.noreply.github.com>
2023-11-06 16:33:39 +0530

    Update Stack.java
    Update the Stack Represenation

--- Stack.java
+++ Stack.java
@@ -1,2 +1,31 @@
 public class Stack {
+  
+    private ListNode top;
+    private int length;
+
+    private class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+    }
+
+    public Stack(){
+        top = null;
+        length = 0;
+    }
+
+    public int length(){
+        return length;
+    }
+
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    public static void main(String [] args){
+
+    }
 }
