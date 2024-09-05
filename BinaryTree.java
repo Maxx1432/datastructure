a8cdcb6a779e6ef2a9abffe07894a92ad51741df
Mahendra Chawla <mahendra.chawla26@gmail.com>
2024-09-05 10:34:47 +0530

    Creating preorder of the tree.

--- BinaryTree.java
+++ BinaryTree.java
@@ -14,22 +14,34 @@ public class BinaryTree {
     }
 
     public void createBinaryTree(){
-        TreeNode first = new TreeNode(1);
+        TreeNode first = new TreeNode(9);
         TreeNode second = new TreeNode(2);
         TreeNode third = new TreeNode(3);
         TreeNode fourth = new TreeNode(4);
         TreeNode fifth = new TreeNode(5);
+        TreeNode sixth = new TreeNode(6);
 
         root = first;         // null<--First-->null
         first.left = second;  // second<--First-->null
         first.right = third;  // second<--First-->third
         second.left = fourth; // fourth<--second-->null
         third.right = fifth;  // null<--third-->fifth
-
+        third.left = sixth;   // sixth<--third-->>fifth
 
     }
 
-    public static void main(String[] args) {
+//    to create preorder of the tree which first visit the left node and then right node
+    public void preOrder(TreeNode root){
+        if(root == null) //base case
+            return;
+        System.out.println(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
 
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.createBinaryTree();
+        bt.preOrder(bt.root);
     }
 }
